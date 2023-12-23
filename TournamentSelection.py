2a431f47a0f635cd5da0f1b4ae5b74ba604a9ae6
Amir Mahdi Sedghi <asedghi1380@gmail.com>
2023-12-23 13:56:04 +0330

    implement tournament selection

--- TournamentSelection.py
+++ TournamentSelection.py
@@ -1,7 +1,10 @@
+import random
+
 from SelectionOperator import SelectionOperator
 
 
 class TournamentSelection(SelectionOperator):
     def select(self, population, fitness):
-        # Tournament selection implementation
-        return
+        tournament_size = 3
+        selected = random.sample(population, tournament_size)
+        return max(selected, key=lambda ind: fitness(ind))
