6fc29907f7d1e76f44b78760960b10c9c7c6832c
Amir Mahdi Sedghi <asedghi1380@gmail.com>
2023-12-23 13:57:02 +0330

    implement single point crossover

--- SinglePointCrossover.py
+++ SinglePointCrossover.py
@@ -1,7 +1,11 @@
+import random
+
 from CrossoverOperator import CrossoverOperator
 
 
 class SinglePointCrossover(CrossoverOperator):
     def crossover(self, parent1, parent2):
-        # Single-point crossover implementation
-        return
+        crossover_point = random.randint(1, min(len(parent1), len(parent2)) - 1)
+        child1 = parent1[:crossover_point] + parent2[crossover_point:]
+        child2 = parent2[:crossover_point] + parent1[crossover_point:]
+        return child1, child2
